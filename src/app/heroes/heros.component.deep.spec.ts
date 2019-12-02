66ea39aa130a95e1da3716d06f5408254347e89e
Arjun kumar verma <arjun.kumar.verma@jci.com>
2019-12-02 20:54:56 +0530

    Testing routerLink test

--- src/app/heroes/heros.component.deep.spec.ts
+++ src/app/heroes/heros.component.deep.spec.ts
@@ -114,4 +114,16 @@ describe('HerosComponent (Deep test)', () => {
       expect(heroText).toContain(name);
     });
 
+    it('should have the correct route for the first hero', () => {
+      mockHeroService.getHeroes.and.returnValues(of(HEROES));
+      fixture.detectChanges();
+
+      const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+      const routerLink = heroComponents[0].query(By.directive(RouterLinkDirectiveStub)).injector.get(RouterLinkDirectiveStub);
+
+      heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+      expect(routerLink.navigateTo).toBe('/detail/1');
+
+    });
+
 });
